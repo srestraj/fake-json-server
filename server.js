e63f79010dceae8cc8c209575c9d863ec5f0d338
Raj Kumar Shrestha <shresthar27@gmail.com>
2022-05-18 23:25:41 +0545

    added auth to server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const jsonServer = require('json-server');
+const auth = require('json-server-auth');
 
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
+server.use(auth);
 server.use(middlewares);
 server.use(router);
 
