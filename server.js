257fd6742aade66300b87c58b0c0b759245855e7
Raj Kumar Shrestha <shresthar27@gmail.com>
2022-05-20 10:48:22 +0545

    added restriction on users route

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const auth = require('json-server-auth');
 
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults({ noCors: true });
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 // bind router db to the app
@@ -12,10 +12,5 @@ server.db = router.db
 server.use(auth);
 server.use(middlewares);
 server.use(router);
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
-    res.header('Access-Control-Allow-Headers', '*')
-    next()
-})
 
 server.listen(port);
