9961710c2676fe56680255a5891d58dd9af070c7
Raj Kumar Shrestha <shresthar27@gmail.com>
2022-05-20 10:56:34 +0545

    added rules to server.js

--- server.js
+++ server.js
@@ -6,9 +6,17 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
+
+// auth rules
+const rules = auth.rewriter({
+    users: 660,
+})
+
+
 // bind router db to the app
 server.db = router.db
 
+server.use(rules);
 server.use(auth);
 server.use(middlewares);
 server.use(router);
